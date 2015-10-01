bb8ba21dade2e43d8585dbd8a982b0553fffae75
elennox <elennox191@gmail.com>
2015-10-01 15:16:33 -0400

    Started 12

---
+++ Euler12/Euler12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool fincheck = false;
+            for(var x = 0; fincheck == false; x++)
+            {
+
+            }
+        }
+    }
+}
