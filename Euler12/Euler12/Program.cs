15fb250fabdf9a14e063c0f6342bd4209bd75dbc
elennox <elennox191@gmail.com>
2015-10-02 14:03:15 -0400

    12 solved

--- Euler12/Euler12/Program.cs
+++ Euler12/Euler12/Program.cs
@@ -8,13 +8,46 @@ namespace Euler12
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             bool fincheck = false;
+            int divcheck = 0;
+            int tri = 0;
             for(var x = 0; fincheck == false; x++)
             {
-
+                tri = tri + x;
+                int divis = getDivisorCount(tri);
+                if(divis > divcheck)
+                {
+                    divcheck = divis;
+                }
+               
+                if (divis >= 500)
+                {
+                    Console.WriteLine(tri + " - " + divis);
+                    fincheck = true;
+                }                
             }
+            Console.ReadLine();
         }
+
+
+        private static int getDivisorCount(int num)
+        {
+            var divCount = 1;
+            for (var x = 1; x <= (int)Math.Sqrt(num); x++)
+            {
+                if(num%x == 0)
+                {
+                    //Console.WriteLine(num + " % " + x + " = " + num % x);
+                    divCount+=2;                    
+                }
+            }
+
+            return divCount;
+        }
+
+
     }
 }
