52586c3731f30aad7c11e213d8fae3b0a6843289
elennox <elennox191@gmail.com>
2015-10-06 17:02:35 -0400

    Euler 13

---
+++ Euler13/Euler13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Euler13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double total = 0;
+            const Int32 BufferSize = 128;
+            using (var fileStream = File.OpenRead("numbers.txt"))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                String line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    total = total + Double.Parse(line);
+                }
+                Console.WriteLine("Total: " + total);
+                Console.ReadLine();
+      // Process line
+  }
+        }
+    }
+}
